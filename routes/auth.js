988b022aa52376734b8c6e81f7cd2c45d9d0dc8a
Maneeshaprabha <maneeprabha28@gmail.com>
2025-03-23 21:43:25 +0530

    login change

--- routes/auth.js
+++ routes/auth.js
@@ -2,43 +2,8 @@ const express = require ("express");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    const { email, password, fullName } = req.body;
+   
   
-    if (!email || !password || !fullName) {
-      return res.status(400).json({ message: "Email, password, and full name are required" });
-    }
-  
-    try {
-      const userRecord = await auth.createUser({
-        email,
-        password,
-        displayName: fullName,
-      });
-  
-      const token = await auth.createCustomToken(userRecord.uid);
-  
-      res.status(201).json({ message: "User registered successfully", token });
-    } catch (error) {
-      res.status(400).json({ message: error.message });
-    }
-  });
-
-  router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-  
-    if (!email || !password) {
-      return res.status(400).json({ message: "Email and password are required" });
-    }
-  
-    try {
-    
-      const user = await auth.getUserByEmail(email);
-      const token = await auth.createCustomToken(user.uid);
-  
-      res.status(200).json({ message: "Login successful", token });
-    } catch (error) {
-      res.status(401).json({ message: "Invalid credentials" });
-    }
   });
   
   module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
-
+const authRoutes = require("./routes/auth");
+// const billRoutes = require("./routes/bills");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: "*",
-    methods: [ 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true 
-  }));
-app.use(bodyParser.json());
+// app.use(cors({
+//     origin: "*",
+//     methods: [ 'POST'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true 
+//   }));
+// app.use(bodyParser.json());
+
+
+
+app.use("/api/auth", authRoutes);
+// app.use("/api/bills", billRoutes);
+
+// Start the server
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
